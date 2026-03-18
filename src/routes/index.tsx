9d2a80009606905defe8af8ea6096f193b2ac703
Sito <40501794+sito8943@users.noreply.github.com>
2026-03-18 10:05:06 +0100

    init

---
+++ src/routes/index.tsx
@@ -0,0 +1,23 @@
+import { createFileRoute } from '@tanstack/react-router'
+import { useState } from 'react'
+
+export const Route = createFileRoute('/')({ component: App })
+
+function App() {
+  const [counter, setCounter] = useState(0)
+  return (
+    <main className="">
+      <section className="flex flex-col gap-10 items-center justify-center h-screen">
+        <h1 className="text-6xl font-bold">M9 tanstack counter</h1>
+        <p>Press in the button to increase the counter</p>
+        <button
+          className="bg-blue-400 font-bold rounded-3xl px-6 py-2"
+          onClick={() => setCounter((prev) => prev + 1)}
+        >
+          Press me!!
+        </button>
+        <p>Counter: {counter}</p>
+      </section>
+    </main>
+  )
+}
